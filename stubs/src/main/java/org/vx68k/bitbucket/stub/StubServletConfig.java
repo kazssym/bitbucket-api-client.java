c83d83f7c4bd2e156f1f2cb5fd027765270bd8cb
Kaz Nishimura <kazssym@vx68k.org>
2018-06-11 10:02:44 +0900

    Make a parameter final

--- stubs/src/main/java/org/vx68k/bitbucket/stub/StubServletConfig.java
+++ stubs/src/main/java/org/vx68k/bitbucket/stub/StubServletConfig.java
@@ -54,7 +54,7 @@ public class StubServletConfig implements ServletConfig
      * {@inheritDoc}
      */
     @Override
-    public String getInitParameter(String name)
+    public String getInitParameter(final String name)
     {
         throw new UnsupportedOperationException("Not supported yet.");
     }
