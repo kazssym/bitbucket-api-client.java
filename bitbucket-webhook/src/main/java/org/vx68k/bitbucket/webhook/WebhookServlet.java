2806965f98c59eeeb15e22ff8c766d7fc801dca5
Kaz Nishimura <kazssym@vx68k.org>
2018-06-12 08:05:19 +0900

    Edit file header comment

--- bitbucket-webhook/src/main/java/org/vx68k/bitbucket/webhook/WebhookServlet.java
+++ bitbucket-webhook/src/main/java/org/vx68k/bitbucket/webhook/WebhookServlet.java
@@ -1,10 +1,10 @@
 /*
- * WebhookServlet
- * Copyright (C) 2014-2015 Nishimura Software Studio
+ * WebhookServlet.java - class WebhookServlet
+ * Copyright (C) 2015-2018 Kaz Nishimura
  *
  * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Affero General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
@@ -14,6 +14,8 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 package org.vx68k.bitbucket.webhook;
