67947dfb2d295b250767fc61d4456806638cec61
Kaz Nishimura <kazssym@vx68k.org>
2018-06-13 23:28:02 +0900

    Update class 'BitbucketClientBranch'

--- bitbucket-api-client/src/main/java/org/vx68k/bitbucket/api/client/BitbucketClientBranch.java
+++ bitbucket-api-client/src/main/java/org/vx68k/bitbucket/api/client/BitbucketClientBranch.java
@@ -1,10 +1,10 @@
 /*
- * BitbucketClientBranch
- * Copyright (C) 2015 Nishimura Software Studio
+ * BitbucketClientBranch.java - class BitbucketClientBranch
+ * Copyright (C) 2015-2018 Kaz Nishimura
  *
  * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Affero General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
@@ -14,6 +14,8 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 package org.vx68k.bitbucket.api.client;
@@ -28,7 +30,7 @@ import javax.json.JsonValue;
 import org.vx68k.bitbucket.api.BitbucketBranch;
 
 /**
- * Branch, named branch, or bookmark represented by a JSON object.
+ * Branch or bookmark represented by a JSON object.
  *
  * @author Kaz Nishimura
  * @since 5.0
@@ -36,53 +38,71 @@ import org.vx68k.bitbucket.api.BitbucketBranch;
 public class BitbucketClientBranch extends BitbucketClientObject
     implements BitbucketBranch
 {
+    /**
+     * Type value for Git branches.
+     */
     private static final String BRANCH = "branch";
+
+    /**
+     * Type value for Mercurial branches.
+     */
     private static final String NAMED_BRANCH = "named_branch";
+
+    /**
+     * Type value for Mercurial bookmarks.
+     */
     private static final String BOOKMARK = "bookmark";
 
-    private static final Logger logger =
-        Logger.getLogger(BitbucketClientBranch.class.getPackage().getName());
-
-    private String name;
-    private List<Commit> heads;
-
-    public BitbucketClientBranch(final JsonObject jsonObject) {
-        super(jsonObject);
-        String entityType = getType();
-        if (!(entityType.equals(BRANCH) || entityType.equals(NAMED_BRANCH)
-                || entityType.equals(BOOKMARK))) {
-            throw new IllegalArgumentException(
-                    "Not \"" + BRANCH + "\", \"" + NAMED_BRANCH + "\", or \""
-                    + BOOKMARK + "\"");
-        }
-        logger.log(
-                Level.FINE, "Parsing JSON object (\"{0}\"): {1}",
-                new Object[] {entityType, jsonObject});
-        name = jsonObject.getString("name");
-        heads = parseCommits(jsonObject.getJsonArray("heads"));
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public List<Commit> getHeads() {
-        return heads;
-    }
-
-    public void setName(final String value) {
-        name = value;
-    }
-
-    public void setHeads(final List<Commit> value) {
-        heads = value;
-    }
-
-    protected static List<Commit> parseCommits(final JsonArray jsonArray) {
+    /**
+     * Parses a JSON array for commits.
+     *
+     * @param commitsArray JSON array of commits
+     * @return list of commits
+     */
+    protected static List<Commit> parseCommits(final JsonArray commitsArray)
+    {
         List<Commit> commits = new ArrayList();
-        for (JsonValue value : jsonArray) {
+        for (JsonValue value : commitsArray) {
             commits.add(new Commit((JsonObject) value));
         }
         return commits;
+    }
+
+    /**
+     * Constructs this branch with a JSON branch object.
+     *
+     * @param branchObject JSON branch object
+     */
+    public BitbucketClientBranch(final JsonObject branchObject)
+    {
+        super(branchObject);
+
+        String type = getType();
+        if (!(type.equals(BRANCH) || type.equals(NAMED_BRANCH) ||
+              type.equals(BOOKMARK))) {
+            throw new IllegalArgumentException("JSON object is not branch");
+        }
+    }
+
+    /**
+     * Returns the name of this branch.
+     *
+     * @return the name
+     */
+    public final String getName()
+    {
+        JsonObject branchObject = getJsonObject();
+        return branchObject.getString("name", null);
+    }
+
+    /**
+     * Returns the heads of this branch.
+     *
+     * @return the heads
+     */
+    public List<Commit> getHeads()
+    {
+        JsonObject branchObject = getJsonObject();
+        return parseCommits(branchObject.getJsonArray("heads"));
     }
 }
