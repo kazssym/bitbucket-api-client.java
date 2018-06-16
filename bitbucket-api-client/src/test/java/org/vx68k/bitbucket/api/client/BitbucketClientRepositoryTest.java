7a82b9ea9e76726e9637b0cb37f9772da97dce32
Kaz Nishimura <kazssym@vx68k.org>
2018-06-16 12:04:31 +0900

    Rename class 'RepositoryTest'

--- bitbucket-api-client/src/test/java/org/vx68k/bitbucket/api/client/BitbucketClientRepositoryTest.java
+++ bitbucket-api-client/src/test/java/org/vx68k/bitbucket/api/client/BitbucketClientRepositoryTest.java
@@ -1,10 +1,10 @@
 /*
- * RepositoryTest
- * Copyright (C) 2015 Nishimura Software Studio
+ * BitbucketClientRepositoryTest.java - class BitbucketClientRepositoryTest
+ * Copyright (C) 2015-2018 Kaz Nishimura
  *
  * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Affero General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
@@ -14,6 +14,8 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 package org.vx68k.bitbucket.api.client;
@@ -24,25 +26,18 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 /**
- * Collection of unit tests for [@link BitbucketClientRepository}.
+ * Unit tests for [@link BitbucketClientRepository}.
  *
  * @author Kaz Nishimura
- * @since 1.0
  */
-public class RepositoryTest {
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
+public class BitbucketClientRepositoryTest
+{
     @Ignore
     @Test
-    public void testDefaultRepository() {
-        BitbucketClientRepository repository = new BitbucketClientRepository(null);
+    public void testDefaultRepository()
+    {
+        BitbucketClientRepository repository =
+            new BitbucketClientRepository(null);
         // TODO: Add test cases.
     }
 }
