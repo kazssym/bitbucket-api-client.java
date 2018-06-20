65b50953de3484a5532348c20db15315038d4915
Kaz Nishimura <kazssym@vx68k.org>
2018-06-20 22:23:13 +0900

    Rewrite drop-down menu handling

--- bitbucket-api-client-example/src/main/webapp/resources/scripts/example.js
+++ bitbucket-api-client-example/src/main/webapp/resources/scripts/example.js
@@ -1,47 +1,39 @@
-/*
-  example.js - custom script for bitbucket-api-client-example
-  Copyright (C) 2015 Nishimura Software Studio
-
-  Copying and distribution of this file, with or without modification, are
-  permitted in any medium without royalty provided the copyright notice and
-  this notice are preserved.  This file is offered as-is, without any warranty.
-*/
-
-/* eslint-env jquery */
+// example.js - app-specific script
+// Copyright (C) 2015-2018 Kaz Nishimura
+//
+// Copying and distribution of this file, with or without modification, are
+// permitted in any medium without royalty provided the copyright notice and
+// this notice are preserved.  This file is offered as-is, without any warranty.
 
 "use strict";
 
-$(document).ready(function () {
-    var accountMenu = $("#account-menu");
-    accountMenu.removeClass("pure-menu-allow-hover");
-    accountMenu.mouseleave(function (event) {
-        $("#account-menu > .pure-menu-children").css("display", "");
-    });
+(() => {
+    const MENU_CLASS_NAME = "pure-menu-allow-hover";
+    const LINK_CLASS_NAME = "pure-menu-link";
 
-    $("#account-menu > .pure-menu-link").click(function (event) {
-        var children = $("#account-menu > .pure-menu-children");
-        if (children.css("display") !== "block") {
-            children.css("display", "block");
-        } else {
-            accountMenu.mouseleave();
+    let menus = document.getElementsByClassName(MENU_CLASS_NAME);
+    for (let menu of menus) {
+        const showChildren = (visible) => {
+            let classList = menu.className.split(/\s+/)
+                .filter((name) => name != MENU_CLASS_NAME);
+            if (visible) {
+                classList.push(MENU_CLASS_NAME);
+            }
+            menu.className = classList.join(" ");
+        };
+
+        showChildren(false);
+        menu.addEventListener("mouseleave", function () {
+                showChildren(false);
+            });
+        for (let child of menu.childNodes) {
+            if (typeof child.className === "string"
+                && child.className.split(/\s+/).includes(LINK_CLASS_NAME)) {
+                child.addEventListener("click", function (event) {
+                        showChildren(true);
+                        event.preventDefault();
+                    });
+            }
         }
-        return false;
-    });
-
-    $(window).resize(function (event) {
-        if ($("#nav-menu").width() !== $(document).width()) {
-            var height = $(document).height();
-            height -= $("#toolbar").height();
-            $("#nav-menu").css("min-height", height + "px");
-        } else {
-            $("#nav-menu").css("min-height", "");
-        }
-    });
-    $(window).resize();
-});
-
-$(window).load(function (event) {
-    // Triggers a <code>resize</code> event now as the document height might
-    // have changed.
-    $(window).resize();
-});
+    }
+})();
