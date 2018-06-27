9521fe0fadda43b075b1041a24e9162594fe609c
Kaz Nishimura <kazssym@vx68k.org>
2018-06-27 10:24:57 +0900

    Add new test class 'BitbucketClientCommitTest'

---
+++ bitbucket-api-client/src/test/java/org/vx68k/bitbucket/api/client/BitbucketClientCommitTest.java
@@ -0,0 +1,45 @@
+/*
+ * BitbucketClientCommitTest.java - class BitbucketClientCommitTest
+ * Copyright (C) 2018 Kaz Nishimura
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package org.vx68k.bitbucket.api.client;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link BitbucketClientCommit}.
+ *
+ * @author Kaz Nishimura
+ */
+public class BitbucketClientCommitTest
+{
+    // @todo Add test methods.
+
+    /**
+     * Tests nothing. This method is a placeholder.
+     */
+    @Test @Ignore
+    public void testNothing()
+    {
+        fail();
+    }
+}
