eb32105d3dbbe2b20a016b757eb3b8b9c844d004
Kaz Nishimura <kazssym@vx68k.org>
2018-06-29 02:10:14 +0900

    Removed annotation 'Named'

--- bitbucket-api-client-example/src/main/java/org/vx68k/bitbucket/api/client/example/User.java
+++ bitbucket-api-client-example/src/main/java/org/vx68k/bitbucket/api/client/example/User.java
@@ -23,7 +23,6 @@ package org.vx68k.bitbucket.api.client.example;
 import java.io.Serializable;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import org.vx68k.bitbucket.api.BitbucketUser;
@@ -36,7 +35,6 @@ import org.vx68k.bitbucket.api.client.BitbucketClient;
  * @since 5.0
  */
 @RequestScoped
-@Named("user")
 public class User implements Serializable
 {
     private static final long serialVersionUID = 1L;
