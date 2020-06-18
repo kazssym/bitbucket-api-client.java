f22d238a08984c0ac252a729ff9af93eb4be72a4
Kaz Nishimura <kazssym@linuxfront.com>
2020-06-18 16:12:28 +0900

    Edit comments

--- bitbucket-client/src/main/java/org/vx68k/bitbucket/client/internal/UUIDAdapter.java
+++ bitbucket-client/src/main/java/org/vx68k/bitbucket/client/internal/UUIDAdapter.java
@@ -1,6 +1,6 @@
 /*
- * ClientUserAccount.java
- * Copyright (C) 2018-2020 Kaz Nishimura
+ * UUIDAdapter.java
+ * Copyright (C) 2020 Kaz Nishimura
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by
