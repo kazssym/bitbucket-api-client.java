e461b42122acb3432d65b8f027516d2d7c7c40c4
Kaz Nishimura <kazssym@linuxfront.com>
2020-06-20 19:20:45 +0900

    Add methods

--- bitbucket-api/src/main/java/org/vx68k/bitbucket/BitbucketBranch.java
+++ bitbucket-api/src/main/java/org/vx68k/bitbucket/BitbucketBranch.java
@@ -1,31 +1,34 @@
 /*
- * BitbucketBranch.java - interface BitbucketBranch
- * Copyright (C) 2018 Kaz Nishimura
+ * BitbucketBranch.java
+ * Copyright (C) 2018-2020 Kaz Nishimura
  *
  * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 package org.vx68k.bitbucket;
 
+import java.util.List;
+
 /**
- * Branch or bookmark in a Bitbucket repository.
+ * Branch, named branch or bookmark in a Bitbucket repository.
  *
  * @author Kaz Nishimura
- * @since 5.0
+ * @since 6.0
  */
 public interface BitbucketBranch extends BitbucketRef
 {
+    List<BitbucketCommit> getHeads();
 }
