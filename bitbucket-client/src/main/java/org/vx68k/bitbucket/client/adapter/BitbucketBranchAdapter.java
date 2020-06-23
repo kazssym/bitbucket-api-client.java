b20a65c23e5fd151384b9d5600fb81fa75c3d495
Kaz Nishimura <kazssym@linuxfront.com>
2020-06-24 07:35:41 +0900

    Add new class 'BitbucketBranchAdapter'

---
+++ bitbucket-client/src/main/java/org/vx68k/bitbucket/client/adapter/BitbucketBranchAdapter.java
@@ -0,0 +1,50 @@
+/*
+ * BitbucketBranchAdapter.java
+ * Copyright (C) 2020 Kaz Nishimura
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package org.vx68k.bitbucket.client.adapter;
+
+import javax.json.bind.adapter.JsonbAdapter;
+import org.vx68k.bitbucket.BitbucketRepository;
+import org.vx68k.bitbucket.client.internal.ClientBranch;
+
+/**
+ * Adapter for {@link BitbucketRepository.Branch}.
+ *
+ * @author Kaz Nishimura
+ * @since 6.0
+ */
+public class BitbucketBranchAdapter
+    implements JsonbAdapter<BitbucketRepository.Branch, ClientBranch>
+{
+    @Override
+    public final ClientBranch adaptToJson(final BitbucketRepository.Branch user)
+    {
+        if (user instanceof ClientBranch) {
+            return (ClientBranch) user;
+        }
+        return new ClientBranch(user);
+    }
+
+    @Override
+    public final BitbucketRepository.Branch adaptFromJson(final ClientBranch user)
+    {
+        return user;
+    }
+}
