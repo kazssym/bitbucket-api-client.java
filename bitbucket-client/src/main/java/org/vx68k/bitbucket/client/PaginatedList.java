678d0eb9cb1624a89ef077160191cab1155840aa
Kaz Nishimura <kazssym@linuxfront.com>
2020-06-25 18:57:11 +0900

    Edit comments

--- bitbucket-client/src/main/java/org/vx68k/bitbucket/client/PaginatedList.java
+++ bitbucket-client/src/main/java/org/vx68k/bitbucket/client/PaginatedList.java
@@ -1,6 +1,6 @@
 /*
  * PaginatedList.java
- * Copyright (C) 2018 Kaz Nishimura
+ * Copyright (C) 2018-2020 Kaz Nishimura
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by
